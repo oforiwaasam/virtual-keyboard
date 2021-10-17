78e4223dbb076a359dfcdb9435ad7e9f31230527
Lily Sam <lilyoforiwaas@gmail.com>
2021-10-16 22:06:44 -0400

    halfway through javascript

--- keyboard/keyboard.js
+++ keyboard/keyboard.js
@@ -16,14 +16,51 @@ const keyboard = {
     },
 
     init() {
+        // Create main elements 
+        this.elements.main = document.createElement("div");
+        this.elements.keysContainer = document.createElement("div");
 
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "1keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+
+        // Add to DOM
+        this.elements.main.appendChild(this.elements.keysContainer);
+        document.body.appendChild(this.elements.main);
     },
 
     _createKeys() {
+        const fragment = document.createDocumentFragment();
+        const keyLayout = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+            "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
+            "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
+            "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+            "space"
+        ];
 
+        // create HTML for an icon
+        const createIconHTML  = (icon_name) => {
+            return '<i class="material-icons">$(icon_name)</i>';
+        }
     },
 
     _triggerEvent(handlerName) {
-        
+        console.log("Event Triggered! Event Name: " + handlerName);
+    },
+
+    _toggleCapsLock() {
+        console.log("Caps Lock Toggled!");
+    },
+
+    open(initialValue, oninput, onclose) {
+
+    },
+
+    close() {
+
     }
 };
+
+window.addEventListener("DOMContentLoaded", function () {
+    keyboard.init();
+});
